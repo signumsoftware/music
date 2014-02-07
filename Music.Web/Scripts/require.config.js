43b566b1702c501a9976b39775de0156a47d7647
Olmo del Corral <olmo@signumsoftware.com>
2014-02-07 09:39:39 +0100

    add promise and update dependencies

--- Music.Web/Scripts/require.config.js
+++
@@ -1,9 +0,0 @@
-/// <reference path="../modules/require.d.ts" />
-
-require.config({
-    baseUrl: 'scripts', // commented for now
-    paths: {
-        "Framework": "..",
-        "Extensions": "..",
-    }
-});
